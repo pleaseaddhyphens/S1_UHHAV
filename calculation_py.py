87edaf979dfdb36ce29779d084102c488f859566
liaisan973 <liaisan973@gmail.com>
2023-12-12 21:21:48 +0300

    added solar panels

--- calculation_py.py
+++ calculation_py.py
@@ -18,6 +18,15 @@ P = 0.0880
 V0 = 20
 pitch_angle = 12
 
+#parametres for calculation solar panels
+power_one_solar_panel = 800
+coef_efficiency = 0.12
+S_one_solar_panel = 1.050036 * 0.540004 #m*m (example of optimised solar panel)
+general_power = 2000
+weight_one_solar_panel = 2 #kg
+curvature = 2/np.pi #for cylindr
+
+
 ##TO DO
 # Number of requared solar panels + surface of them + energy genearted + curved surface 
 # thrust force from the propeller analysis 
@@ -55,6 +64,37 @@ def drag_force_propeller(pitch_angle,d, P, V0, RPM):
     F = P * A * (B - D) * C
     return F
 
+def curved_surface(Surface_area):
+#     S_surface = 2 * np.pi * (radius_airship ** 2) + 2 * np.pi * radius_airship * length_airship
+      #
+    S_curved_area = Surface_area * curvature
+    return np.round(S_curved_area)
+
+Surface_curved_area = curved_surface(Surface_area)
+
+
+def numbers_solar_panels(power_one_solar_panel, coef_efficiency, general_power):
+    N = general_power / (power_one_solar_panel * coef_efficiency)
+    return np.round(N)
+
+N = numbers_solar_panels(power_one_solar_panel, coef_efficiency, general_power)
+
+def surface_solar_panels(N, S_one_solar_panel, curvature):
+    S_solar_panels = S_one_solar_panel * N * curvature
+    return np.round(S_solar_panels)
+
+def weight_solar_panels(N, weight_one_solar_panel):
+    weight_solar_panels = weight_one_solar_panel * N
+    return(np.round(weight_solar_panels))
+
+
 propeller_drag = drag_force_propeller(pitch_angle, d, P, V0, RPM)
+# numbers = numbers_solar_panels(power_one_solar_panel, coef_efficiency, general_power)
+Surface_solar_panels = surface_solar_panels(N, S_one_solar_panel, curvature)
+Weight_solar_panels = weight_solar_panels(N, weight_one_solar_panel)
 print(f"Propeller drag force: {propeller_drag}, N")
 print(f"Availible mass for floating in the {alt} m altitude: {np.round(m,3)} kg")
+print(f"Surface of curved area: {Surface_curved_area} m^2")
+print(f"Numbers of solar panels: {N}")
+print(f"Surface of solar panels: {Surface_solar_panels} m^2")
+print(f"Weight of solar panels: {Weight_solar_panels} kg")
