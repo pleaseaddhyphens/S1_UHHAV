a4618ebc819e2e054a16c2eee5363530cb00f3e1
pleaseaddhyphens <148846512+pleaseaddhyphens@users.noreply.github.com>
2023-12-12 16:26:52 +0300

    add drag calculation

--- calculation_py.py
+++ calculation_py.py
@@ -11,13 +11,20 @@ Velocity = 50 #[m/s]
 Ref_area = (Volume_streamlined_shape)**(2/3) #[m^s]
 Density_air = 1.293 #[kg/m^-3]
 
+# varianle for propeller drag force coefs
+RPM = 3500
+d = 103
+P = 0.0880
+V0 = 20
+pitch_angle = 12
+
 ##TO DO
-# Volume and surfecae of the shape of GNVR 
-# calculation of the drag force (analitical) + CFD results
 # Number of requared solar panels + surface of them + energy genearted + curved surface 
 # thrust force from the propeller analysis 
 # No more then 100 kg, 8kW 
 # Structural mass calculator 
+# Volume and surface of the shape of GNVR 
+
 
 def Drag_force(Density_air, Velocity,Ref_area, C_d ): 
  
@@ -39,4 +46,15 @@ def lift(v, h):
 
 m = lift(Volume_streamlined_shape, alt)
 
+def drag_force_propeller(pitch_angle,d, P, V0, RPM):
+    pitch = np.abs(np.pi * 0.75 * d * np.tan(pitch_angle))
+    A = (np.pi * ((0.0254 * d)**2))/4
+    B = (RPM * 0.0254 * pitch * (1/60))**2
+    C = (d/(3.29546 * pitch))**1.5
+    D = (RPM * 0.0254 * pitch * 1/60)* V0
+    F = P * A * (B - D) * C
+    return F
+
+propeller_drag = drag_force_propeller(pitch_angle, d, P, V0, RPM)
+print(f"Propeller drag force: {propeller_drag}, N")
 print(f"Availible mass for floating in the {alt} m altitude: {np.round(m,3)} kg")
